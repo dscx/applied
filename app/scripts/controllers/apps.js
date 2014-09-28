65360d501e44ad64ad4e7d10a558b20879eff1aa
Dan Chamberlain <dnchmbrln@gmail.com>
2014-09-27 20:54:28 -0700

    completed basic add functionality and local storage

--- app/scripts/controllers/apps.js
+++ app/scripts/controllers/apps.js
@@ -10,28 +10,44 @@
 angular.module('appliedApp')
   .controller('AppsCtrl', function ($scope) {
     $scope.testForms = [
-      {'Company': 'Apple', 'DateApplied': new Date()},
-      {'Company': 'Miscorosft', 'DateApplied': new Date()},
-      {'Company': 'Sweet Startup', 'DateApplied': new Date()}
+      {'company': 'Apple', 'DateApplied': 'Jan 1st', 'Link': 'www.apple.com', 'description': 'Mac genius', 'id': 12836},
+      {'company': 'Microsoft', 'DateApplied': 'Jan 2nd', 'Link': 'www.microsoft.com', 'description': 'Software Engineer', 'id': 17728},
+      {'company': 'Sweet Startup', 'DateApplied': 'Jan 3rd', 'Link': 'www.google.com', 'description': 'Software Engineer', 'id': 92036}
     ];
 
     $scope.applications = {}
+    $scope.applications.entries = {};
+    $scope.applications.loaded = false;
 
     $scope.submitForm = function(isValid) {
-      $scope.master = {}
-
+      var app = {}
       if (isValid) {
-        //window.localStorage.setItem('forms', JSON.stringift($scope.applications))
-        $scope.name = '';
-        $scope.user.date = '';
-        $scope.link = '';
-        $scope.id = '';
-        $scope.description = ''
-        console.log('saved!')
+        app.company = $scope.name 
+        app.date = $scope.user.date
+        app.link = $scope.link
+        app.id = $scope.id
+        app.description = $scope.description
+        $scope.applications.entries[app.company] = angular.copy(app);
+        console.log(app, 'saved!')
+        window.localStorage.setItem('apps', JSON.stringify($scope.applications.entries))
       }
 
     };
 
+
+    $scope.LoadData = function(){
+          var saved = window.localStorage.getItem('apps');
+          if($scope.applications.loaded === false){
+            if(saved !== 'undefined'){
+              saved = JSON.parse(saved);
+              angular.copy(saved, $scope.applications.entries);
+              console.log('Data Loaded!')
+              $scope.applications.loaded = true;
+            }
+          }
+        };
+
+      $scope.LoadData();
   });
 
 
